1a629cb0e50d3c4124344ab91afdf36f619c5d4f
zeduard89 <zeduard89@gmail.com>
2023-07-13 21:36:44 -0300

    fix: responsive update,4

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,7 +2,7 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div className='animate-slideDown  flex justify-center  py-3  space-x-2 sm:space-x-10 bg-slate-800 rounded-br-[80px] rounded-bl-[80px]  mx-5 '>
+    <div className='animate-slideDown  flex justify-center  py-3  space-x-1 sm:space-x-10 bg-slate-800 rounded-br-[80px] rounded-bl-[80px]  mx-5 '>
       <button className='btnNav bg-green-600 hover:bg-green-700 text-white font-bold sm:w-36 w-24 rounded-tl-3xl rounded-br-3xl '>
         <NavLink className="lg:text-2xl" to='/'>About Me</NavLink>
       </button>
