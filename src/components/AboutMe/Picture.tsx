897ce6ec4f86ba78363b7c0a071cbe18314fe292
zeduard89 <zeduard89@gmail.com>
2023-07-18 17:08:56 -0300

    feat : picture text

--- src/components/AboutMe/Picture.tsx
+++ src/components/AboutMe/Picture.tsx
@@ -3,17 +3,24 @@ import vidPerfil from '../utils/AboutMe/Rec 0011.mp4'
 
 const Picture = () => {
   return (
-    <div className='  grid grid-cols-5 mt-5 pt-[13rem]'>
-        <div className='col-span-2'>
-            <h2>hola</h2>
+    <div className='  grid grid-cols-2 md:grid-cols-5 mt-5 pt-[13rem]'>
+        <div className='hidden md:block col-span-2 text-center'>
+            <h1>designer</h1>
+            <p>UI/UX Diseñador apasionado</p>
+            <p>enfoado en la experiencia</p>
+            <p>de usuario</p>
         </div>
-        <div className=' drop-shadow-xl place-self-center m-5 col-span-1'>
+        <div className=' drop-shadow-xl place-self-center m-5 md:col-span-1'>
             <div className="  rounded-[999rem] h-[16rem] w-[14rem]  overflow-hidden ">
               <video src={vidPerfil} autoPlay loop muted className='w-full h-full object-cover' />
             </div>       
         </div>
-        <div className="col-span-2">
-            <h2>hola</h2>
+        <div className="hidden md:block col-span-2 text-center">
+            <h1>coder</h1>
+            <p>Desarrollador</p>
+            <p>Front End y Back End</p>
+            <p>Enfocado en un codigo</p>
+            <p>limpio y eficiente</p>
         </div>
     </div>
     )
